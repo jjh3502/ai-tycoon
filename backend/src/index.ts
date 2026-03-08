ded55e30da44c94914c5c401b9cbffc3c16bf073
jujeonghyeon <162390517+jujeonghyeon@users.noreply.github.com>
2026-03-08 20:51:36 +0900

    fix: CORS에 DELETE 메서드 추가

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(
       env.NODE_ENV === 'development'
         ? ['http://localhost:5173', 'http://127.0.0.1:5173']
         : [],
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   }),
 )
